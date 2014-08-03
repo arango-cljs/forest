49cbc5e5f39fa832b8be57ba7085586c2b94f02c
Hoàng Minh Thắng <p@banphim.net>
2014-08-03 11:55:43 +0700

    add basic wrapper around arango/foxx

---
+++ src-cljs/foxx_cljs/arango.cljs
@@ -0,0 +1,36 @@
+(ns foxx-cljs.arango
+  (:refer-clojure :exclude [print])
+  (:require [foxx-cljs.core :refer [*url-prefix*]]))
+
+(def ^:private logger (js/require "console"))
+
+(defn log [& data] (.log logger (pr-str data)))
+
+(def ^:private internal (js/require "internal"))
+
+(defn print [& data] (.print internal (pr-str data)))
+
+(def foxx-auth (js/require "org/arangodb/foxx/authentication"))
+(def arango-is (js/require "org/arangodb/is"))
+(def errors (.-errors internal))
+(def Sessions (.-Sessions foxx-auth))
+(def Users (.-Users foxx-auth))
+(def CookieAuthentication (.-CookieAuthentication foxx-auth))
+(def Authentication (.-Authentication foxx-auth))
+
+(def Foxx (js/require "org/arangodb/foxx"))
+
+(def FormatMiddleware (.-FormatMiddleware Foxx))
+
+(def Controller (.-Controller Foxx))
+
+(def Model (.extend (.-Model Foxx) #js {}))
+
+(defn Repository [indexes]
+  (.extend (.-Repository Foxx) indexes))
+
+(defn start [routes]
+  (let [initial-app (->> #js {:urlPrefix *url-prefix*}
+                         (Controller. js/applicationContext))]
+    ;; assiociate routes to app
+    (routes initial-app)))
