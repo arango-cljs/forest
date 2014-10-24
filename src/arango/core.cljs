f873c3ad3acd551d907474b0ee316184ecf20d3d
Hoàng Minh Thắng <p@banphim.net>
2014-10-24 22:56:32 +0700

    add arango.core

---
+++ src/arango/core.cljs
@@ -0,0 +1,25 @@
+(ns arango.core)
+
+(def ^{:no-doc true :private true}
+  internal (js/require "internal"))
+
+(def ^{:doc "is true if being in development mode, otherwise false."}
+  development-mode? (.-developmentMode internal))
+
+(def ^{:doc "A string representing current platform."}
+  platform (.-platform internal))
+
+(def ^{:doc "A string representing ArangoDB version."}
+  version (.-version internal))
+
+(def Foxx (js/require "org/arangodb/foxx"))
+
+(def FormatMiddleware (.-FormatMiddleware Foxx))
+
+(def Controller (.-Controller Foxx))
+
+(defn start [routes & {:keys [url-prefix]}]
+  (let [initial-app (->> (when url-prefix #js {:urlPrefix url-prefix})
+                         (Controller. js/applicationContext))]
+    ;; assiociate routes to app
+    (routes initial-app)))
