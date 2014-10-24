3048676e0686a1b2966d10e28ee0129c33d44224
Hoàng Minh Thắng <p@banphim.net>
2014-10-24 23:10:29 +0700

    add forest.route.cljs

---
+++ src/forest/route.cljs
@@ -0,0 +1,29 @@
+(ns forest.route
+  "Functions and macros to help building Compojure-like request
+  handlers."
+  (:require [forest.response :refer [response]]))
+
+(def ^:dynamic *app* #js {})
+(def ^:dynamic *request* #js {})
+(def ^:dynamic *response* #js {})
+
+(defn routing
+  "Apply a list of routes to a Foxx app."
+  [app & handlers]
+  (binding [*app* app]
+    (some #(% app) handlers)))
+
+(defn routes
+  "Create a Foxx handler by combining several handlers into one."
+  [& handlers]
+  #(apply routing % handlers))
+
+(defn any
+  "Shorthand for requests that match any HTTP method."
+  [app uri handler]
+  (.get app uri handler)
+  (.post app uri handler)
+  (.put app uri handler)
+  (.del app uri handler)
+  (.head app uri handler)
+  (.patch app uri handler))
